13b2d6713714f3d9715298346561307c63b64d9f
S-jianye <shao378367249@126.com>
2016-07-01 22:07:44 +0800

    jianye分支,1.1

---
+++ RunningNote/RunningNote/Controllers/RDynamicTableVC.h
@@ -0,0 +1,13 @@
+//
+//  RDynamicTableVC.h
+//  RunningNote
+//
+//  Created by qingyun on 16/7/1.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDynamicTableVC : UITableViewController//跑友动态
+
+@end
