76cf12f89d386cd0f742f5613ec087cceb4c2de9
GaoHongBin <1119752812@qq.com>
2016-07-06 09:28:47 +0800

    7.6AM

---
+++ RunningNote/RunningNote/Controllers/RRegisterVC.m
@@ -0,0 +1,37 @@
+//
+//  RRegisterVC.m
+//  RunningNote
+//
+//  Created by qingyun on 16/7/6.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import "RRegisterVC.h"
+
+@interface RRegisterVC ()
+
+@end
+
+@implementation RRegisterVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
