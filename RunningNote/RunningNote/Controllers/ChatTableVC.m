2cc894aa17e4dce7900ed394e7a733182e8883f3
Sjianye <shao378367249@126.com>
2016-07-21 21:17:53 +0800

    跟上一个提交一样,2

--- RunningNote/RunningNote/Controllers/ChatTableVC.m
+++ RunningNote/RunningNote/Controllers/ChatTableVC.m
@@ -24,7 +24,6 @@
     
     
     // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
